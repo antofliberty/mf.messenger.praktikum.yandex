3e0a918a04cdbb7f20226435940e4b7dcec5673d
antofliberty <onfern@gmail.com>
2020-08-19 12:59:59 +0300

    comment

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const port = 3000
 // Все файлы и так лежат в нужной директории, по сути весь проект это статика
 app.use('/', express.static(__dirname + '/public/'))
 
-app.get('/', function(req, res) {
-    res.sendfile('public/index.html');
-});
-
-app.get('/404', function(req, res) {
-    res.sendfile('public/404.html');
-});
-
-app.get('/500', function(req, res) {
-    res.sendfile('public/500.html');
-});
-
-app.get('/chat', function(req, res) {
-    res.sendfile('public/chat.html');
-});
-
-app.get('/login', function(req, res) {
-    res.sendfile('public/login.html');
-});
-app.get('/signup', function(req, res) {
-    res.sendfile('public/signup.html');
-});
-app.get('/settings', function(req, res) {
-    res.sendfile('public/settings.html');
-});
+// app.get('/', function(req, res) {
+//     res.sendfile('public/index.html');
+// });
+//
+// app.get('/404', function(req, res) {
+//     res.sendfile('public/404.html');
+// });
+//
+// app.get('/500', function(req, res) {
+//     res.sendfile('public/500.html');
+// });
+//
+// app.get('/chat', function(req, res) {
+//     res.sendfile('public/chat.html');
+// });
+//
+// app.get('/login', function(req, res) {
+//     res.sendfile('public/login.html');
+// });
+// app.get('/signup', function(req, res) {
+//     res.sendfile('public/signup.html');
+// });
+// app.get('/settings', function(req, res) {
+//     res.sendfile('public/settings.html');
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
