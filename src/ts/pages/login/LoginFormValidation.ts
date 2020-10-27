73b4983dd46f12854834794076b580a733e8d407
antofliberty <onfern@gmail.com>
2020-10-27 06:07:20 +0300

    working

--- src/ts/pages/login/LoginFormValidation.ts
+++ src/ts/pages/login/LoginFormValidation.ts
@@ -1,9 +1,8 @@
 export default document.addEventListener('DOMContentLoaded', ()=>{
-    console.log(this)
     let form = document.getElementById('login-form')
-// @ts-ignore
+    // @ts-ignore
     let fields = document.getElementsByClassName('login-form__input')
-// @ts-ignore
+    // @ts-ignore
     form.onsubmit = validate
 
     let email = fields[0];
@@ -16,7 +15,7 @@ export default document.addEventListener('DOMContentLoaded', ()=>{
     (<HTMLInputElement>email).onfocus = reset;
     (<HTMLInputElement>password).onfocus = reset;
 
-    function validate(e: Event) {
+    function validate(e: any) {
         e.preventDefault()
 
         let isEmailValid = validateEmail()
@@ -26,18 +25,12 @@ export default document.addEventListener('DOMContentLoaded', ()=>{
             (<HTMLFormElement>form).submit()
         }
 
-        if(!isEmailValid) {
-            makeRed(undefined, email)
-        }
 
-        if(!isPasswordValid) {
-            makeRed(undefined, password)
-        }
     }
 
 
 
-    function validateEmail(): boolean {
+    function validateEmail(e?: Event): boolean {
         let match = Boolean(emailValue.match("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])"))
         if(match) {
             reset()
@@ -47,24 +40,28 @@ export default document.addEventListener('DOMContentLoaded', ()=>{
         return match;
     }
 
-    function validatePassword(): boolean {
+    function validatePassword(e?: Event): boolean {
         let match = Boolean(passwordValue.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"))
         if(match) {
-            reset()
+            reset(e)
         } else {
-            makeRed()
+            makeRed(e)
         }
         return match;
     }
 
-    function makeRed(e?: any, el?: Element): void {
-        if (e) el = e.target
+    function makeRed(e: any): void {
+        let el = e.target
         // @ts-ignore
         el.style.border = "1px solid red"
     }
 
-    function reset(e?: any, el?: Element): void {
-        if (e) el = e.target
+    function reset(e?: any): void {
+        if (e) {
+            let el = e.target
+        } else {
+
+        }
         // @ts-ignore
         el.style.border = "none"
     }
